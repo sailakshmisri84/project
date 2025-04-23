643882db0bbddaef7a9782ca369a8a010cba0171
sailakshmisri84 <sailakshmisri.d@gmail.com>
2025-04-23 11:15:14 +0530

    Update pandemic.js

--- pandemic.js
+++ pandemic.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tips = [
-  "Before: Stay informed about the disease and vaccines.",
-  "During: Wear masks and practice social distancing.",
-  "During: Wash hands frequently and avoid gatherings.",
-  "After: Continue hygiene practices and monitor symptoms.",
-  "After: Support recovery by staying updated and cautious."
-];
-  
-const tipsList = document.getElementById("tipsList");
+    "Before: Stay informed about the disease and vaccines.",
+    "During: Wear masks and practice social distancing.",
+    "During: Wash hands frequently and avoid gatherings.",
+    "After: Continue hygiene practices and monitor symptoms.",
+    "After: Support recovery by staying updated and cautious."
+  ];
+
+  const tipsList = document.getElementById("tipsList");
   tips.forEach(tip => {
     const li = document.createElement("li");
     li.textContent = tip;
     tipsList.appendChild(li);
   });
 });
-
